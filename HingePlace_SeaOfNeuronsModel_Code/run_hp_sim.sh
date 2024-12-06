ea302186060b6778d59c7435a977d3acc0bbf06f
chaitanyagoswami <chaitanya.goswami96@gmail.com>
2024-12-05 22:04:24 -0500

    Add files via upload

--- HingePlace_SeaOfNeuronsModel_Code/run_hp_sim.sh
+++ HingePlace_SeaOfNeuronsModel_Code/run_hp_sim.sh
@@ -1,8 +1,9 @@
-python3 HingePlace_sim.py 2 0
-python3 HingePlace_sim.py 1 0
-python3 HingePlace_sim.py 3 0
-python3 HingePlace_sim.py 2 1 
-python3 HingePlace_sim.py 3 1
-python3 HingePlace_sim.py 3 2
-python3 HingePlace_sim.py 3 3
-python3 HingePlace_sim.py 3 4
+python3 find_opt_dir_HP.py # Finding optimum direction and threshold
+python3 HingePlace_sim.py 2 0 # Running Sec 5-2-1
+python3 HingePlace_sim.py 1 0 # Running Sec 5-2-2
+python3 HingePlace_sim.py 3 0 # Running Sec 5-2-3
+python3 HingePlace_sim.py 2 1 # Running Sec 5-2-4
+python3 HingePlace_sim.py 3 1 # Running Sec 5-2-4 Target 1
+python3 HingePlace_sim.py 3 2 # Running Sec 5-2-4 Target 3
+python3 HingePlace_sim.py 3 3 # Running Sec 5-2-4 Target 3
+python3 HingePlace_sim.py 3 4 # Running Sec 5-2-4 Target 4
